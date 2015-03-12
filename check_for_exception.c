d6ef1cb2558b1239840b5d1e43004b08f0aabe6e
mokolodi1 <mokolodi1@gmail.com>
2015-03-12 14:53:28 +0100

    classy not trashy

--- check_for_exception.c
+++ check_for_exception.c
@@ -18,15 +18,6 @@
 ** it.
 */
 
-static int			found_exception(t_operator **solution, int *solution_length
-									, t_operator first)
-{
-	*solution_length = 1;
-	*solution = malloc(*solution_length * sizeof(t_operator));
-	(*solution)[0] = first;
-	return (1);
-}
-
 int					check_for_exception(int length, int *numbers
 										, t_operator **solution
 										, int *solution_length)
@@ -37,17 +28,24 @@ int					check_for_exception(int length, int *numbers
 	ft_quicksort(length, sorted);
 	if (length >= 2 && numbers[1] == sorted[0] && sorted[1] == numbers[0]
 		&& ft_memcmp(sorted + 2, numbers + 2, (length - 2) * sizeof(int)) == 0)
-		return (found_exception(solution, solution_length, SWAP_A));
+	{
+		*solution_length = 1;
+		*solution = malloc(*solution_length * sizeof(t_operator));
+		(*solution)[0] = SWAP_A;
+		return (1);
+	}
 	if (length >= 5 && numbers[length - 1] == sorted[length - 2]
 		&& sorted[length - 1] == numbers[length - 2]
 		&& ft_memcmp(sorted, numbers, (length - 2) * sizeof(int)) == 0)
 	{
-		*solution = malloc(5 * sizeof(t_operator));
+		*solution_length = 5;
+		*solution = malloc(*solution_length * sizeof(t_operator));
 		(*solution)[0] = REVERSE_ROTATE_A;
 		(*solution)[1] = REVERSE_ROTATE_A;
 		(*solution)[2] = SWAP_A;
 		(*solution)[3] = ROTATE_A;
 		(*solution)[4] = ROTATE_A;
+		return (1);
 	}
 	return (0);
 }
