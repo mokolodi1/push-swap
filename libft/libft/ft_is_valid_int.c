8ab75e695778a16bd2737577b8d13be5583bbba9
Teo Fleming <tfleming@student.42.fr>
2015-03-13 13:24:14 +0100

    libft norme

--- libft/libft/ft_is_valid_int.c
+++ libft/libft/ft_is_valid_int.c
@@ -1,12 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_is_valid_int.c                                  :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: tfleming <tfleming@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2015/03/13 13:22:09 by tfleming          #+#    #+#             */
+/*   Updated: 2015/03/13 13:23:41 by tfleming         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
 #include "libft.h"
 
+/*
+** is_just_bad formatted like that because of the Norme.
+*/
+
 static int			is_just_bad(char *string)
 {
-	return (string[0] == '\0'
-			|| (string[0] == '-' && string[1] == '\0')
-			|| (string[0] == '-' && !ft_isdigit(string[1]))
-			|| (string[0] != '-' && !ft_isdigit(string[0])));
+	int				to_return;
+
+	to_return = (string[0] == '\0'
+					|| (string[0] == '-' && string[1] == '\0')
+					|| (string[0] == '-' && !ft_isdigit(string[1]))
+					|| (string[0] != '-' && !ft_isdigit(string[0])));
+	return (to_return);
 }
 
 static void			set_is_negative(int *is_negative, char **string)
